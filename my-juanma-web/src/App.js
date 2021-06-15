fe91e7b6c975d3d3a098219102b0210332457dfc
juanma19911991 <85264602+juanma19911991@users.noreply.github.com>
2021-06-15 00:03:08 -0300

    add component

--- my-juanma-web/src/App.js
+++ my-juanma-web/src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavbarComponent } from './Components/Navbar/Navbar';
+import "./Components/Navbar/Navbar.css"
 
 function App() {
 
-  const shopName = 'JuanmaWeeb';
+  
 
-  return (
-    <div className="App">
-      <header className="Bar">
-        <p className="Logo">{shopName}</p>
-      <nav className="Nav">
-        <ul className="Links">
-          <li className="Link"><a href="">Sector</a></li>
-          <li className="Link"><a href="">Sector</a></li>
-          <li className="Link"><a href="">Sector</a></li>
-          <li className="Link"><a href="">Sector</a></li>
-        </ul>
-      </nav>
-      </header>
-    </div>
+  return ( 
+    <NavbarComponent />
+   
   )
 }
 
---
+++ my-juanma-web/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+export const NavbarComponent =  ()=>  {
+    const shopName = 'JuanmaWeeb';
+    return(
+    <div className="App">
+    <header className="Bar">
+      <p className="Logo">{shopName}</p>
+    <nav className="Nav">
+      <ul className="Links">
+        <li className="Link"><a href="">Sector</a></li>
+        <li className="Link"><a href="">Sector</a></li>
+        <li className="Link"><a href="">Sector</a></li>
+        <li className="Link"><a href="">Sector</a></li>
+      </ul>
+    </nav>
+    </header>
+  </div>
+    )
+}
+
