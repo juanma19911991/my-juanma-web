6bc98a38ee44249d1085041a263f155d9c29ef98
juanma19911991 <85264602+juanma19911991@users.noreply.github.com>
2021-06-19 20:15:42 -0300

    entrega 4 contador

--- my-juanma-web/src/App.js
+++ my-juanma-web/src/App.js
@@ -4,6 +4,9 @@ import { NavbarComponent } from './Components/Navbar/Navbar.jsx';
 import "./Components/Navbar/Navbar.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ItemListConteiner } from './Conteiners/ItemListConteiner';
+import { CardComponent } from './Components/Cards/Card';
+
+
 
 
 
@@ -13,12 +16,14 @@ function App() {
 
   return (
 
+    
     <div>
       <NavbarComponent />
       <div>
         <ItemListConteiner greeting={"Hola1234"} />
       </div>
 
+      <CardComponent />
     </div>
 
   )
